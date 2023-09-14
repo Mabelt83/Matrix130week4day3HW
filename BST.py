3664bba9666d48eb370b433ff59d76ab45577af5
Mabelt83 <137824865+Mabelt83@users.noreply.github.com>
2023-09-14 09:52:33 -0400

    homework

---
+++ BST.py
@@ -0,0 +1,76 @@
+from Node import Node
+
+class BinarySearchTree:
+
+  def __init__(self, root_value):
+    self.root = Node(root_value)
+
+  def __repr__(self):
+    return f'<BST: {self.root}'
+  
+  def add_node(self, value, current_node=None):
+    if not current_node:
+      current_node = self.root
+    if value > current_node.value:
+      if current_node.right:
+        self.add_node(value, current_node.right)
+      else:
+        current_node.right = Node(value)
+    elif value < current_node.value:
+      if current_node.left:
+        self.add_node(value, current_node.left)
+      else:
+        current_node.left = Node(value)
+
+  def search(self, value, current_node=None):
+    if not current_node:
+      current_node = self.root
+    if value > current_node.value:
+      if current_node.right:
+        return self.search(value, current_node.right)
+    elif value < current_node.value:
+      if current_node.left:
+        return self.search(value, current_node.left)
+    else:
+      return current_node
+    print(f'Node: {value} not found')
+    return False
+
+  def get_min(self):
+    current_node = self.root
+    while current_node.left:
+       current_node = current_node.left
+    return current_node
+       
+
+  def get_max(self):
+    current_node = self.root
+    while current_node.right:
+       current_node = current_node.right
+    return current_node
+    
+
+  def print_sorted(self, current_node = None):
+    if not current_node:
+       current_node = self.root
+    if current_node.left:
+       self.print_sorted(current_node.left)
+    print(current_node.value)
+    if current_node.right:
+       self.print_sorted(current_node.right)
+
+
+    
+    
+
+bst = BinarySearchTree(100)
+bst.add_node(125)
+bst.add_node(130)
+bst.add_node(115)
+bst.add_node(50)
+bst.add_node(75)
+bst.add_node(75)
+
+print(bst.root.right.right)
+print(bst.root.right.left)
+print(bst.root.left.right)
---
+++ Node.py
@@ -0,0 +1,31 @@
+class Node:
+
+  def __init__(self, value):
+    self.value = value
+    self.right = None
+    self.left = None
+ 
+  def __repr__(self):
+    return f'<Node: {self.value}>'
+
+#node = Node('Monday')
+
+
+#node2 = Node('Tuesday')
+
+#node.right = node2
+
+
+#node3 = Node('Wednesday')
+
+#node2.right = node3
+
+#rint(node, node.right.right)
+
+    
+
+
+
+
+
+
